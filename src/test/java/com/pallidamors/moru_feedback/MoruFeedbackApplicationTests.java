396a343ef6df936f33d4a2d2878dd6b623953d2b
upright_help <pallidamors8250@gmail.com>
2025-09-24 23:14:27 +0900

    [최정우] MORU_Feedback 프로젝트 생성 기본 세팅 완료

---
+++ src/test/java/com/pallidamors/moru_feedback/MoruFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pallidamors.moru_feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoruFeedbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
